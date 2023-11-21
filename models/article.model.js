eb328cfa8d44e59d48b63a9d78a6c535d6f32181
matt12171 <matthewpatterson@hotmail.co.uk>
2023-11-21 13:38:23 +0000

    Remove unused string building

--- models/article.model.js
+++ models/article.model.js
@@ -1,13 +1,9 @@
 const db = require('../db/connection')
 
 exports.selectArticleById = (id) => {
-    let queryStr = `SELECT * FROM articles `
-
-    if (id) {
-        queryStr += `WHERE article_id = $1 `
-    }
-
-    return db.query(queryStr, [id])
+    return db.query(
+        `SELECT * FROM articles
+        WHERE article_id = $1;`, [id])
         .then((response) => {
             if (response.rows.length === 0) {
                 return Promise.reject({ status: 400, msg: 'bad request' })
